24f51ef364ce4e137e7d1bcc0db9f74308dc77c6
Vijay Sharma <login2github@gmail.com>
2023-09-18 20:33:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# print("hello baby")
+from fastapi import FastAPI
+from src.module import func
+
+app = FastAPI()
+
+@app.get('/')
+def first_page():
+    return func()
+
+# if __name__ == "__main__":
+#     app.
+
+
+
